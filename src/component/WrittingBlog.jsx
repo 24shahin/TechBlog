7a06944214e217bc1325bb619243def02d98f5fa
Md. Zahid-Hossain Shahin <126140511+24shahin@users.noreply.github.com>
2024-09-03 19:31:20 +0600

    submit

--- src/component/WrittingBlog.jsx
+++ src/component/WrittingBlog.jsx
@@ -18,7 +18,9 @@ function WrittingBlog() {
   return (
     <div className="mt-16">
       <div>
-        <h2 className="text-center text-3xl font-bold mb-8">Write What Makes You Different From Others</h2>
+        <h2 className="text-center text-3xl font-bold mb-8">
+          Write What Makes You Different From Others
+        </h2>
         <div className="grid gap-6 mb-6 ">
           <div className="w-[600px] mx-auto">
             <label
@@ -32,7 +34,6 @@ function WrittingBlog() {
               id="first_name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Summary"
-             
               onChange={handleChange}
               name="summary"
             />
@@ -107,7 +108,6 @@ function WrittingBlog() {
               id="website"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Others Description"
-             
               onChange={handleChange}
               name="otherdescription"
             />
@@ -135,6 +135,11 @@ function WrittingBlog() {
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-28 mx-auto"
             onClick={handlesubmit}
+            disabled={
+              writtingData.tittle=="" && writtingData.description == ""
+                ? true
+                : false
+            }
           >
             Submit
           </button>
